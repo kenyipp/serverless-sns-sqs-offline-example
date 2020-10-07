3d6f4f59b67d9236c3273ba2acc5e4766f08259e
Biruk Haileye Tabor <biruk.haileye@gmail.com>
2020-10-07 15:16:43 +0300

    fix typo: destionation -> destination

--- src/actionDispatcher.js
+++ src/actionDispatcher.js
@@ -6,10 +6,10 @@ const logger = require("../utils/logger");
 const awsHelper = require("../utils/awsHelper");
 
 const bodySchema = Joi.object({
-	destionation: Joi
+	destination: Joi
 		.string()
 		.allow(["firstQueue", "secondQueue", "lambdaFunction"])
-		.description("The destionation you want to forward the payload to")
+		.description("The destination you want to forward the payload to")
 		.required(),
 	payload: Joi
 		.object()
@@ -37,7 +37,7 @@ async function handler(event) {
 
 		await sns.publish({
 			Message: JSON.stringify(body),
-			TopicArn: awsHelper.SNS.getArn(body.destionation),
+			TopicArn: awsHelper.SNS.getArn(body.destination),
 		}).promise();
 
 		return {};
