1985f52788e6f7c23b5a507904b059c0fec662f6
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-02-18 22:14:19 +0000

    update docs

--- src/messageReceiver.js
+++ src/messageReceiver.js
@@ -1,7 +1,7 @@
 "use strict";
 
 async function handler(event) {
-	console.log(JSON.stringify(event, null, 4));
+	console.log(JSON.stringify("FINISHED", event, null, 4));
 }
 
 module.exports.handler = handler;
